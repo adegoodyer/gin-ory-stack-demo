8691b51febe4baf6b147a8c448d18d0bf49ee46d
Adrian Goodyer <adriangoodyer@gmail.com>
2024-02-07 14:51:37 +0000

    Initial router configuration

--- server/router.go
+++ server/router.go
@@ -1,6 +1,17 @@
 package server
 
 import (
+	"encoding/gob"
+	accountrecovery "gin-ory-stack-demo/controllers/account-recovery"
+	accountverification "gin-ory-stack-demo/controllers/account-verification"
+	"gin-ory-stack-demo/controllers/dashboard"
+	profilesettings "gin-ory-stack-demo/controllers/profile-settings"
+	signin "gin-ory-stack-demo/controllers/sign-in"
+	signup "gin-ory-stack-demo/controllers/sign-up"
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -21,7 +32,17 @@ func init() {
 }
 
 func NewRouter() *gin.Engine {
-	router := gin.Default()
+	// gin.DisableConsoleColor()
+	gin.SetMode(gin.DebugMode)
+	// gin.SetMode(gin.ReleaseMode)
+	router := gin.Default() // default includes logger and crash recovery middlewares
+
+	// register custom types to store in cookies
+	gob.Register(map[string]interface{}{})
+
+	// session and cookie management
+	store := cookie.NewStore([]byte("secret"))
+	router.Use(sessions.Sessions("auth-session", store))
 
 	// middlewares
 	router.Use(func(ctx *gin.Context) {
@@ -30,12 +51,28 @@ func NewRouter() *gin.Engine {
 		ctx.Next()
 	})
 
-	// routes
+	// static routes
+	router.Static("/static", "web/static")
+	router.LoadHTMLGlob("web/template/**/*")
+
+	// app routes
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.Redirect(http.StatusMovedPermanently, "/dashboard")
+	})
+	router.GET("/dashboard", dashboard.Handler)
+	router.GET("/sign-in", signin.Handler)
+	router.GET("/sign-up", signup.Handler)
+	router.GET("/account-recovery", accountrecovery.Handler)
+	router.GET("/account-verification", accountverification.Handler)
+	router.GET("/profile-settings", profilesettings.Handler)
 	router.GET("/metrics", gin.WrapH(promhttp.Handler()))
-	router.GET("/ping", func(context *gin.Context) {
-		context.JSON(200, gin.H{
-			"message": "pong",
-		})
+
+	// error handling
+	router.NoRoute(func(ctx *gin.Context) {
+		ctx.Redirect(http.StatusMovedPermanently, "/error")
+	})
+	router.NoMethod(func(ctx *gin.Context) {
+		ctx.Redirect(http.StatusMovedPermanently, "/error")
 	})
 
 	return router
